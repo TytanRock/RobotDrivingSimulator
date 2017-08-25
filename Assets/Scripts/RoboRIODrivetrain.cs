c7e56271dc180609313ca811fcfde4791be58bdf
TytanRock <nesscory@gmail.com>
2017-08-24 21:13:27 -0400

    Initial commit

---
+++ Assets/Scripts/RoboRIODrivetrain.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoboRIODrivetrain : MonoBehaviour {
+
+    public SwerveDrive script;
+
+    const float wheelBase = 0.8f;
+    const float trackBase = 0.8f;
+    //float R = Mathf.Sqrt((wheelBase * wheelBase) + (trackBase * trackBase));
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float y = -Input.GetAxis("Forward");
+        float x = Input.GetAxis("Strafe");
+        float t = Input.GetAxis("Turn");
+
+        deadband(ref y);
+        deadband(ref x);
+        deadband(ref t);
+
+        float A = x - t * (wheelBase / trackBase);
+        float B = x + t * (wheelBase / trackBase);
+        float C = y - t * (trackBase / wheelBase);
+        float D = y + t * (trackBase / wheelBase);
+
+        float ws1 = Mathf.Sqrt(Mathf.Pow(B, 2) + Mathf.Pow(C, 2)); //Front Right
+        float ws2 = Mathf.Sqrt(Mathf.Pow(B, 2) + Mathf.Pow(D, 2)); //Front Left
+        float ws3 = Mathf.Sqrt(Mathf.Pow(A, 2) + Mathf.Pow(D, 2)); //Rear Left
+        float ws4 = Mathf.Sqrt(Mathf.Pow(A, 2) + Mathf.Pow(D, 2)); //Rear Right
+
+        float wa1 = Mathf.Atan2(B, C) * 180 / Mathf.PI; //Front Right
+        float wa2 = Mathf.Atan2(B, D) * 180 / Mathf.PI; //Front Left
+        float wa3 = Mathf.Atan2(A, D) * 180 / Mathf.PI; //Rear Left
+        float wa4 = Mathf.Atan2(A, C) * 180 / Mathf.PI; //Rear Right
+
+        float max = Mathf.Max(new float[] { ws1, ws2, ws3, ws4 });
+        if (max > 1)
+        {
+            ws1 = ws1 / max;
+            ws2 = ws2 / max;
+            ws3 = ws3 / max;
+            ws4 = ws4 / max;
+        }
+
+        script.leftBackAngle = wa3;
+        script.rightBackAngle = wa4;
+        script.leftFrontAngle = wa2;
+        script.rightFrontAngle = wa1;
+
+        script.leftBackPower = ws3;
+        script.rightBackPower = ws4;
+        script.leftFrontPower = ws2;
+        script.rightFrontPower = ws1;
+    }
+
+    void deadband(ref float value)
+    {
+        if (Mathf.Abs(value) < 0.05)
+            value = 0;
+    }
+}
---
+++ Assets/Scripts/SwerveDrive.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwerveDrive : MonoBehaviour {
+
+    public float leftBackAngle;
+    public float rightBackAngle;
+    public float leftFrontAngle;
+    public float rightFrontAngle;
+
+    public float leftBackPower;
+    public float rightBackPower;
+    public float leftFrontPower;
+    public float rightFrontPower;
+
+    WheelCollider backLeft;
+    WheelCollider backRight;
+    WheelCollider frontLeft;
+    WheelCollider frontRight;
+
+    public float power = 500;
+
+	// Use this for initialization
+	void Start () {
+        backLeft = transform.GetChild(0).GetComponent<WheelCollider>();
+        backRight = transform.GetChild(1).GetComponent<WheelCollider>();
+        frontLeft = transform.GetChild(2).GetComponent<WheelCollider>();
+        frontRight = transform.GetChild(3).GetComponent<WheelCollider>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        backLeft.motorTorque = leftBackPower * power;
+        backRight.motorTorque = rightBackPower * power;
+        frontLeft.motorTorque = leftFrontPower * power;
+        frontRight.motorTorque = rightFrontPower * power;
+
+        if (leftBackPower < 0.05)
+        {
+            backLeft.motorTorque = 0;
+            backLeft.brakeTorque = 0.1f;
+        }
+        else
+        {
+            backLeft.brakeTorque = 0;
+        }
+        if (leftFrontPower < 0.05)
+        {
+            frontLeft.motorTorque = 0;
+            frontLeft.brakeTorque = 0.1f;
+        }
+        else
+        {
+            frontLeft.brakeTorque = 0;
+        }
+        if (rightBackPower < 0.05)
+        {
+            backRight.motorTorque = 0;
+            backRight.brakeTorque = 0.1f;
+        }
+        else
+        {
+            backRight.brakeTorque = 0;
+        }
+        if (rightFrontPower < 0.05)
+        {
+            frontRight.motorTorque = 0;
+            frontRight.brakeTorque = 0.1f;
+        }
+        else
+        {
+            frontRight.brakeTorque = 0;
+        }
+
+
+        backLeft.steerAngle = leftBackAngle;
+        backRight.steerAngle = rightBackAngle;
+        frontLeft.steerAngle = leftFrontAngle;
+        frontRight.steerAngle = rightFrontAngle;
+
+        //transform.GetChild(0).rotation = Quaternion.Euler(0, leftBackAngle, 0);
+        //transform.GetChild(1).rotation = Quaternion.Euler(0, rightBackAngle, 0);
+        //transform.GetChild(2).rotation = Quaternion.Euler(0, leftFrontAngle, 0);
+        //transform.GetChild(3).rotation = Quaternion.Euler(0, rightFrontAngle, 0);
+    }
+}
